150f2e3901cb4cc84770ee42e940a7df57cc69da
Abhishek Tripathi <54246710+abhi7585@users.noreply.github.com>
2020-03-31 17:08:36 +0530

    Update data.py

--- HangmanVersion2/data/data.py
+++ HangmanVersion2/data/data.py
@@ -3,20 +3,20 @@ import numpy as np
 import random
 
 # Reading data set
-df1 = pd.read_csv('.\data\wordsDataSet.csv')
+df1 = pd.read_csv(r'C:\Users\abhi7\Downloads\Hangman-Version-2-master\Hangman-Version-2-master\HangmanVersion2\data\wordsDataSet.csv')
 # Removed Count coloumn as it was not required
 df1 = df1.drop('count', 1)
 # Added length column to remove words with small length
 df1['length'] = df1.word.str.len()
 df1 = df1[df1.length > 6.0]
 
-df2 = pd.read_csv('.\data\CountriesName.csv')
+df2 = pd.read_csv(r'C:\Users\abhi7\Downloads\Hangman-Version-2-master\Hangman-Version-2-master\HangmanVersion2\data\CountriesName.csv')
 # Removing irrelevant columns
 df2.drop(['Region', 'Population', 'Area (sq. mi.)', 'Pop. Density (per sq. mi.)', 'Coastline (coast/area ratio)', 'Net migration', 'Infant mortality (per 1000 births)', 'GDP ($ per capita)',
           'Literacy (%)', 'Phones (per 1000)', 'Arable (%)', 'Crops (%)', 'Other (%)', 'Climate', 'Birthrate', 'Deathrate', 'Agriculture', 'Industry', 'Service'], axis=1, inplace=True)
 
 # Removing irrelevant columns
-df3 = pd.read_csv('.\data\ColorNames.csv')
+df3 = pd.read_csv(r'C:\Users\abhi7\Downloads\Hangman-Version-2-master\Hangman-Version-2-master\HangmanVersion2\data\ColorNames.csv')
 df3.drop(['hex_24_bit', 'red_8_bit', 'green_8_bit', 'blue_8_bit',
           'hue_degrees', 'hsl_s', 'hsl_l'], axis=1, inplace=True)
 
